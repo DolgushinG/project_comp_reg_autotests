9e1def325dd173ac6bd5bd3a6fa208010f0fba50
g.dolgushin <g.dolgushin@mcplat.ru>
2024-05-16 15:54:47 +0300

    project update

--- locators/ProfileLocators.py
+++ locators/ProfileLocators.py
@@ -5,7 +5,7 @@ class Profile:
     nav_profile = (By.ID, 'overview')
     title_profile = (By.XPATH, '//h6[contains(., \'Команда\')]')
     nav_events = (By.ID, 'events')
-    title_events = (By.XPATH, "//h5[contains(., 'Вы не принимали участие ни в каких соревнованиях')]")
+    title_events = (By.XPATH, "//*[contains(., 'Вы еще не принимали участие в соревнованиях')]")
     exist_title_events = (By.CSS_SELECTOR, "h2[id='heading6']")
     nav_settings = (By.ID, 'setting')
     title_settings = (By.XPATH, '//label[contains(., \'Старый пароль\')]')
