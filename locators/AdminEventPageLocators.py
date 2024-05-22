19e4457e348bdf303531cd6ec8136c1ee9c31529
g.dolgushin <g.dolgushin@mcplat.ru>
2024-05-22 12:48:09 +0300

    project update

--- locators/AdminEventPageLocators.py
+++ locators/AdminEventPageLocators.py
@@ -36,7 +36,7 @@ class AdminEventPageLocators:
     title_success_create = (By.XPATH, '//div[contains(., "Успешно сохранено!")]')
     image = (By.CSS_SELECTOR, 'input[type="file"]')
     description_text_area = (By.XPATH, '//*[contains(@class, \'note-editable\')]')
-    classic_radio_btn = (By.XPATH, '//*[contains(@class, "is_qualification_counting_like_final0")]')
+    classic_radio_btn = (By.XPATH, '//*[contains(@class, "is_france_system_qualification0")]')
     all_route_radio_btn = (By.XPATH, '//div[contains(@class, \'mode2\')]')
     btn_add_category = (By.XPATH, "//div[contains(@class, 'categories-add')]")
     field_category_1 = (By.XPATH, "(//input[contains(@name, 'categories[values][]')])[1]")
