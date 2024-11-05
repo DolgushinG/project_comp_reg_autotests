42d3a9f43ac9c031454644c887bc5dff0b156672
g.dolgushin <g.dolgushin@mcplat.ru>
2024-11-05 16:59:43 +0300

    project update

--- helpers/repeat_fill_field.py
+++ helpers/repeat_fill_field.py
@@ -1,7 +1,4 @@
 from selenium.webdriver.common.by import By
-from tomlkit import value
-
-from conftest import PROJECT_ROOT
 from constants import URL
 from pages.AdminEventPage import AdminEventPage
 
@@ -15,12 +12,4 @@ def repeat_fill_fields(driver):
     admin_event_form.fill_field_dinamic(field='address', value="Москва")
     admin_event_form.fill_field_dinamic(field='[name="dates[start_date]"]', type=By.CSS_SELECTOR ,value="20-04-2024")
     admin_event_form.fill_field_dinamic(field='[name="dates[end_date]"]',type=By.CSS_SELECTOR ,value="20-04-2026")
-    # admin_event_form.upload_image(f"{PROJECT_ROOT}/image.jpg")
-    # admin_event_form.fill_field_description(value="long text")
-    # admin_event_form.fill_field_dinamic(field='contact', value="79992200222")
-    # admin_event_form.scroll_up()
-    # admin_event_form.click_btn_tab_pay()
-    # admin_event_form.fill_field_dinamic(field='link_payment', value="https://ya.ru")
-    # admin_event_form.fill_field_dinamic(field='amount_start_price', value="1200")
-    # admin_event_form.fill_field((By.XPATH,'(//div[contains(@class, "note-editable")])[2]'), "text")
     return admin_event_form
