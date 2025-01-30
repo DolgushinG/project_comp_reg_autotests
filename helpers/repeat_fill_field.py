e02f7da0b42098666cc02a09d16e5c63a5987261
g.dolgushin <g.dolgushin@mcplat.ru>
2025-01-30 18:49:13 +0300

    project update

--- helpers/repeat_fill_field.py
+++ helpers/repeat_fill_field.py
@@ -5,11 +5,11 @@ from pages.AdminEventPage import AdminEventPage
 
 def repeat_fill_fields(driver):
     admin_event_form = AdminEventPage(driver)
-    admin_event_form.go_to_url(f'{URL}/moon/resource/event-resource/event-form-page')
+    admin_event_form.go_to_url(f'{URL}moon/resource/event-resource/form-page/37')
     admin_event_form.fill_field_dinamic(field='field-event-resource-6', value="Фестиваль 2024")
     admin_event_form.fill_field_dinamic(field='field-event-resource-7', value="skalodrom")
     admin_event_form.fill_field_dinamic(field='field-event-resource-8', value="Москва")
     admin_event_form.fill_field_dinamic(field='field-event-resource-10', value="Москва")
-    admin_event_form.fill_field_dinamic(field='field-event-resource-11', type=By.ID ,value="20-04-2024")
-    admin_event_form.fill_field_dinamic(field='field-event-resource-12', type=By.ID ,value="20-04-2026")
+    admin_event_form.fill_field_dinamic(field='field-event-resource-11', value="20-04-2024")
+    admin_event_form.fill_field_dinamic(field='field-event-resource-12', value="20-04-2026")
     return admin_event_form
